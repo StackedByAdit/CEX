618256e12627d8c547bbe0d3e48590fd7c0186f1
Aditya <adityaguptavns1@gmail.com>
2026-05-07 05:12:44 +0530

    add schema

---
+++ backend/src/schemas/zodSchema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+    username : z.string().min(3, "uesrname too short"),
+    password : z.string().min(6, "password too short")
+})
+
+export const loginSchema = z.object({
+    username : z.string(),
+    password : z.string()
+})
+
+export const orderSchema = z.object({
+    userId : z.number(),
+    side : z.enum(["BUY", "SELL"]),
+    type : z.enum(["LIMIT", "MARKET"]),
+    symbol : z.string(),
+    price : z.number().optional(),
+    quantity : z.number()
+
+})
