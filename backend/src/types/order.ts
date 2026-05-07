d4a8f99dc176bea0f8d7dd3bf2742751263fe159
Aditya <adityaguptavns1@gmail.com>
2026-05-07 10:55:56 +0530

    add order type for in-memory order

---
+++ backend/src/types/order.ts
@@ -0,0 +1,11 @@
+export type MemoryOrder = {
+    id : string,
+    userId : string,
+    side : "BUY" | "SELL",
+    type : "LIMIT" | "MARKET",
+    symbol : string,
+    price? : number,
+    quantity : number,
+    filledQuantity : number,
+    status : "PENDING" | "PARTIALLY_FILLED" | "FILLED" | "CANCELLED"
+}
