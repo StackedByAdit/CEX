edb74c11cca7c3e703fa976bda0cac18068d5409
Aditya <adityaguptavns1@gmail.com>
2026-05-16 09:33:57 +0530

    fix : assureBalance recognize stock

--- backend/src/utils/assureBalance.ts
+++ backend/src/utils/assureBalance.ts
@@ -2,42 +2,51 @@ import { prisma } from "../prisma";
 
 export async function assureBalance(userId: string, symbol: string) {
 
+    if (symbol === "INR") {
+        let balance = await prisma.balance.findFirst({
+            where: {
+                userId,
+                assetType: "INR",
+            }
+        });
+
+        if (!balance) {
+            balance = await prisma.balance.create({
+                data: {
+                    userId,
+                    stockId: null,
+                    assetType: "INR",
+                    available: 0,
+                    locked: 0
+                }
+            });
+        }
+
+        return balance;
+    }
+
+    const stock = await prisma.stock.findUnique({
+        where: { symbol }
+    });
+
+    if (!stock) {
+        throw new Error(`Stock not found: ${symbol}`);
+    }
+
     let balance = await prisma.balance.findFirst({
         where: {
             userId,
-            assetType: symbol === "INR" ? "INR" : "STOCK",
-            stock: symbol === "INR"
-                ? undefined
-                : {
-                    symbol
-                }
+            assetType: "STOCK",
+            stockId: stock.id
         }
     });
 
     if (!balance) {
-
-        let stockId: string | null = null;
-
-        if (symbol !== "INR") {
-
-            const stock = await prisma.stock.findUnique({
-                where: {
-                    symbol
-                }
-            });
-
-            if (!stock) {
-                throw new Error("Stock not found");
-            }
-
-            stockId = stock.id;
-        }
-
         balance = await prisma.balance.create({
             data: {
                 userId,
-                stockId,
-                assetType: symbol === "INR" ? "INR" : "STOCK",
+                stockId: stock.id,
+                assetType: "STOCK",
                 available: 0,
                 locked: 0
             }
