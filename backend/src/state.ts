9c8d1cd289858e7eb1d59ffedb3c84d6d3daa28f
Aditya <adityaguptavns1@gmail.com>
2026-05-22 00:10:09 +0530

    add balances state

--- backend/src/state.ts
+++ backend/src/state.ts
@@ -2,6 +2,8 @@ import type { MemoryOrder } from "./types/order";
 
 export const ORDERS: MemoryOrder[] = [];
 
+export const BALANCES: Record<string, Record<string, { available: number, locked: number, balanceId: string }>> = {};
+
 export const ORDERBOOK: Record<string, { asks: Record<number, MemoryOrder[]>; bids: Record<number, MemoryOrder[]> }> = {
     AXIS: { asks: {}, bids: {} },
     SOL: { asks: {}, bids: {} },
